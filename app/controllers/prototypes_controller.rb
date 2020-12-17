d52d811999fdc9b0b418b435803f9f9adf40eae8
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-17 14:52:45 +0900

    修正：詳細ページの既存投稿の置き換えの件

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,7 +33,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    if Prototype.update(prototype_params)
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
